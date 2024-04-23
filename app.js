b0fdaf5d1ebc6b26706570e39e051c543b859a0e
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 01:15:04 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+const scores = [
+  { score: 90, subject: 'HTML', weight: 0.2 },
+  { score: 95, subject: 'CSS', weight: 0.3 },
+  { score: 85, subject: 'JavaScript', weight: 0.5 }
+];
+
+function weightedAverage(scores) {
+  let sumScoreTimesWeight = scores.reduce((acc, item) => acc + (item.score * item.weight), 0);
+  let sumWeights = scores.reduce((acc, item) => acc + item.weight, 0);
+  return sumScoreTimesWeight / sumWeights;
+}
+
+console.log(weightedAverage(scores)); 
